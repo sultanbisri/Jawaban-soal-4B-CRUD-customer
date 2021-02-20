976b91787464d97cc8c3549bb1c40faf3eb8db18
sultanbisri <79346157+sultanbisri@users.noreply.github.com>
2021-02-20 17:39:25 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -30,7 +30,7 @@ $orang = query("SELECT * FROM customer WHERE id = $id");
             <a href="ubah.php?id=<?= $orang['id']; ?>">ubah</a> |
             <a href="hapus.php?id=<?= $orang['id']; ?>" onclick="return confirm('Apakah Anda Yakin Ingin Menghapus Data ?'); ">hapus</a>
         </li>
-        <li><a href="index.php">Kembali Ke Daftar Mahasiswa</a></li>
+        <li><a href="index.php">Kembali Ke Daftar Customer</a></li>
     </ul>
 
 </body>
